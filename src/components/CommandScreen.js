5e9497e132a327947e84b95fb7f5d6fb0b1d5282
s3818520 <s3818520@rmit.edu.vn>
2022-04-28 17:25:24 +0700

    ui fixed

--- src/components/CommandScreen.js
+++ src/components/CommandScreen.js
@@ -342,7 +342,7 @@ export default function CommandScreen() {
 
             } else if (cmd == "SAVE") {
 
-                
+
 
             } else if (cmd == "RESTORE") {
 
@@ -386,7 +386,7 @@ export default function CommandScreen() {
 
                 <div className="card-footer text-center">
                     <div className="input-group input-group-sm mb-3">
-                        &gt;Ledis <input type="text" className="form-control ms-2" id="myInput" onKeyDown={handleKeyDown} onChange={(e) => { setInput(e.target.value) }} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"></input>
+                        &gt;Ledis <input type="text" className="form-control ms-2" placeholder="Enter your command..." style={{border:0 ,outline:0 }} id="myInput" onKeyDown={handleKeyDown} onChange={(e) => { setInput(e.target.value) }} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"></input>
                     </div>
                     <button type="button" className="btn btn-secondary" onClick={() => setCommandList([])}>CLEAR COMMAND BOX</button>
                 </div>
